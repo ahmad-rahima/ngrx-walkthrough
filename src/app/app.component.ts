7a03fdfdc2587690afa2b44d4421c83cf808f74f
Ahmad Rahima <ahmadrahimaim@gmail.com>
2023-09-18 23:52:35 +0300

    Finished app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectBookCollection, selectBooks } from './state/books.selectors';
+import { BookApiActions, BooksActions } from './state/books.actions';
+import { GoogleBooksService } from './book-list/books.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngrx-walkthrough';
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  constructor(
+    private bookService: GoogleBooksService,
+    private store: Store
+  ) {}
+
+  onAdd(bookId: string) {
+    this.store.dispatch(BooksActions.addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
+  }
+
+  ngOnInit() {
+    this.bookService
+      .getBooks()
+      .subscribe(books => {
+        this.store.dispatch(BookApiActions.retrievedBookList({ books }));
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { booksReducer } from './state/books.reducers';
 import { collectionReducer } from './state/collection.reducer';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookCollectionComponent,
   ],
   imports: [
     BrowserModule,
